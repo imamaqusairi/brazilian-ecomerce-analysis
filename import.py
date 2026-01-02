3418ba64697db78e3afd403d24d600c1af85b993
imamaqusairi <imam.aqusairi@gmail.com>
2026-01-02 15:27:53 +0700

    new file:   import.py

---
+++ import.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
+
+print(r"C:\Users\MSI THIN\Documents\Projects\Brazillian E Commerce Dataset", path)
+
