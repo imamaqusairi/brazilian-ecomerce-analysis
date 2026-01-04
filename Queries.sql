079eb4f1be6600dda57a83b2d3ac999f5d0553cc
imamaqusairi <imam.aqusairi@gmail.com>
2026-01-04 23:03:08 +0700

    Solved several problems

--- Queries.sql
+++ Queries.sql
@@ -1,14 +1,14 @@
 -- Current Situation (Jan - Oct 2018): 
--- 1. Total Order
--- 2. Total Gross Merchandising Value (GMV)
--- 3. Total Net Revenue 
--- 4. Average Order Value (AOV)
+-- 1. Total Order -- Done
+-- 2. Total Gross Merchandising Value (GMV) -- Done
+-- 3. Total Net Revenue -- Done
+-- 4. Average Order Value (AOV) -- Done
 -- 5. Retention Rate (1 Year)
 -- 6. 3 Top City Contributor (Based on GMV)
 -- 7. 3 Top States Contributor (Based on GMV)
 -- 8. 5 Most Favored Product 
 
--- -- Total Order
+-- Total Order
 -- SELECT 
 -- 	COUNT 
 -- 		(DISTINCT order_id) AS total_order
@@ -23,3 +23,39 @@
 -- USING order_purchase_timestamp::TIMESTAMP;
 
 -- Total Gross Merchandising Value (GMV)
+-- SELECT 
+-- 	SUM(table1."price") AS GMV 
+-- FROM 
+-- 	public."order item" table1
+-- INNER JOIN 
+-- 	public."order" table2
+-- ON
+-- 	table1."order_id" = table2."order_id"
+-- WHERE 	
+-- 	table2."order_purchase_timestamp" BETWEEN '2018-01-01 00:00:00' AND '2018-12-31 23:59:59'
+
+
+-- Total Net Revenue (Total Price - Total Freight)
+-- SELECT 
+-- 	(SUM(table1.price) - SUM(table1.freight_value)) "Net REVENUE"
+-- FROM
+-- 	public."order item" table1
+-- INNER JOIN
+-- 	public."order" table2
+-- ON 
+-- 	table1."order_id" = table2."order_id"
+-- WHERE
+-- 	table2."order_purchase_timestamp" BETWEEN '2018-01-01 00:00:00' AND '2018-12-31 23:59:59'
+
+
+-- Average Order Value (AOV)
+-- SELECT 
+-- 	(SUM(table1.price) / COUNT(table1."order_id")) "Average Order Value"
+-- FROM 
+-- 	public."order item" table1
+-- INNER JOIN
+-- 	public."order" table2
+-- ON
+-- 	table1."order_id" = table2."order_id"
+-- WHERE
+-- 	table2."order_purchase_timestamp" BETWEEN '2018-01-01 00:00:00' AND '2018-12-31 23:59:59'
